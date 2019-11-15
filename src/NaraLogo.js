1c404c8fde03160e2da70c13ec7f487cbc70bf3d
btopro <btopro@outlook.com>
2019-11-15 17:37:16 -0500

    some documentation updates and made it reactive to property changes

--- src/NaraLogo.js
+++ src/NaraLogo.js
@@ -1,33 +1,59 @@
 import { html, css, LitElement } from 'lit-element';
 
 export class NaraLogo extends LitElement {
+  /**
+   * LitElement life cycle - css render
+   */
   static get styles() {
-    return css`
+    return [css`
       :host {
         display: block;
-        padding: 5px;
+        padding: var(--nara-logo-padding, 5px);
       }
-    `;
+    `];
   }
-
+  /**
+   * LitElement / popular convention
+   */
   static get properties() {
     return {
-      stacked: { type: String },
-      horizontal: { type: String },
-      eagle: { type: String },
+      format: { type: String },
+      __logo: { type: String },
     };
   }
-
+  /**
+   * HTMLElement
+   */
   constructor() {
     super();
-    this.stacked = '/nara-logo-stack.svg';
-    this.horizontal = '/nara-logo-horizontal.svg';
-    this.eagle = '/nara-logo-eagle.svg';
+    this.format = 'stack';
   }
-
+  /**
+   * LitElement life cycle - render
+   */
   render() {
     return html`
-      <img src="${this.horizontal}" />
+      <img src="${this.__logo}" alt="" />
     `;
+  }
+  /**
+   * LitElement life cycle - property changed
+   */
+  updated(changedProperties) {
+    changedProperties.forEach((oldValue, propName) => {
+      if (propName == 'format') {
+        switch (this[propName]) {
+          case 'stack':
+          case 'horizontal':
+          case 'eagle':
+            this.__logo = `/nara-logo-${this[propName]}.svg`;
+            break;
+          // this way if someone screws it up it still does something
+          default:
+            this.__logo = `/nara-logo-stack.svg`;
+            break;
+        }
+      }
+    });
   }
 }
